5d2ec377dd06f6c5d8991eabada04fcdab425cb2
Adrian-Ionut Sefcic <adriansefcic@yahoo.ro>
2023-07-19 13:58:48 +0300

    team section responsive

--- components/PlayerCard.tsx
+++ components/PlayerCard.tsx
@@ -1,37 +1,33 @@
-import Image from "next/image"
+import Image from "next/image";
 
 type IPlayerCardProps = {
   name: string;
   position: string;
   description: string;
   image: string;
-}
+};
 
 const PlayerCard = (props: IPlayerCardProps) => {
   return (
-    <div className="mx-12 mt-12 sm:grid sm:grid-cols-2 border-2 p-6 border-black teamcard rounded-xl text-gray-900 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
-      
-      <div className="mr-6 text-center">
-        <div className="text-xl font-bold my-1">
-          {props.name}
+    <div className="w-full flex justify-center">
+      <div className="mt-12 border-2 p-2 sm:p-4 inline-block border-black teamcard rounded-xl text-gray-900 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+        <div className="text-center w-40 sm:w-48">
+          <div className="text-xl font-bold my-1">{props.name}</div>
+          <div className="my-1">{props.position}</div>
+          <div className="text-sm sm:text-base">{props.description}</div>
         </div>
-        <div className="my-1">{props.position}</div>
-        <div>{props.description}</div>
-      </div>
 
-      <div className="justify-self-center h-2/3">
-        <div className="mt-[77px] w-[70px] h-[35px] sm:w-[130px] sm:h-[71px] bg-texture bg-cover absolute rounded-tl-[2rem] rounded-br-[2rem] rounded-bl-lg rounded-tr-lg">
-
-        </div>
-        <img
+        <div className="justify-self-center my-auto flex justify-center">
+          <div className="mt-[60px] sm:mt-[77px] w-[110px] h-[47px] sm:w-[120px] sm:h-[57px] bg-texture bg-cover absolute rounded-tl-[2rem] rounded-br-[2rem] rounded-bl-lg rounded-tr-lg"></div>
+          <img
             src={props.image}
             alt="poza_jucator"
-            className="w-[110px] relative z-10 ml-3"
+            className="w-[80px] sm:w-[100px] relative z-10 ml-3"
           />
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
--- sections/Team.tsx
+++ sections/Team.tsx
@@ -1,39 +1,67 @@
-import PlayerCard from "@/components/PlayerCard"
+import PlayerCard from "@/components/PlayerCard";
 
 const Team = () => {
   return (
-    <section id="echipa" className='py-16 bg-gradient-to-br from-gray-100 to-[#fed9bc]'>
-        <div className='sectiontitle pt-8'>
-          echipă
-        </div>
-        <div className="sm:grid sm:grid-cols-2 grid-flow-row mx-0 sm:mx-64 mb-32 mt-10">
-          <PlayerCard
-          name = "Paul Ciucă"
-          position = "Atacant"
-          description ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
-          image = "/assets/images/ciucanobg.png"
-          />
-          <PlayerCard
-          name = "Paul Ciucă"
-          position = "Atacant"
-          description ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
-          image = "/assets/images/ciucanobg.png"
-          />
-          <PlayerCard
-          name = "Paul Ciucă"
-          position = "Atacant"
-          description ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
-          image = "/assets/images/ciucanobg.png"
-          />
-          <PlayerCard
-          name = "Paul Ciucă"
-          position = "Atacant"
-          description ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
-          image = "/assets/images/ciucanobg.png"
-            />
-        </div>
-      </section>
-  )
-}
+    <section
+      id="echipa"
+      className="py-16 bg-gradient-to-br from-gray-100 to-[#fed9bc]"
+    >
+      <div className="sectiontitle pt-8">echipă</div>
+      <div
+        className="mb-32 mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4
+        lg:bg-white xl:bg-red-500 md:bg-black sm:bg-green-400"
+      >
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+        <PlayerCard
+          name="Paul Ciucă"
+          position="Atacant"
+          description="ALorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sed."
+          image="/assets/images/ciucanobg.png"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Team
+export default Team;
